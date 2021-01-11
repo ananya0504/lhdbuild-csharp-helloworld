5536b1b3ea46fded8d717eb7ec6b0016ec0f42d7
ananya0504 <ananyagupta0504@gmail.com>
2021-01-11 21:27:25 +0530

    Created Console app

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello MLH Local Hack Day - Build!");
+            Console.WriteLine("I kinda already know that I'm the best!?");
+            var name= "Ananya";
+            Console.WriteLine($"I am {name}!");
+        }
+    }
+}
